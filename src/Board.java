1c53c94e4f5b16bc5efcd148d3fc7e128cc5ed8e
jiaQiu9 <351470636@qq.com>
2024-09-15 16:57:49 -0400

    finished implementation of Box Class. In Board Class,implememted the creatBoardPieces() that assigns Box objects to the board with unique id

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,35 @@
 public class Board {
     private int Turn;
-    private int[][] Board; //Maybe a seperate class for each box on the board
+    private Box[][] Board; //Maybe a seperate class for each box on the board
     private int[] pieces;
     private String[][] playerWinLoss;
     private String[][] moves;
+    private int boardSizeM; //store the row number of the board
+    private int boardSizeN; //store the column number of the board
 
+    public Board(int boardM, int boardN){
+        this.boardSizeM=boardM;
+        this.boardSizeN=boardN;
+
+        this.Board=new Box[this.boardSizeM][this.boardSizeN];
+
+    }
+    public void createBoardPiece(){
+        int count = this.getBoardSizeM() * this.getBoardSizeN();
+        int current = 0;
+        for (int i=0;  i<this.getBoardSizeM();i++){
+            for (int j=0; j<this.getBoardSizeN(); j++){
+                if (current<count){
+                    this.Board[i][j]= new Box(current) ;
+                    current++;
+                }
+            }
+        }
+    }
+    public int getBoardSizeM(){
+        return this.boardSizeM;
+    }
+    public int getBoardSizeN(){
+        return this.boardSizeN;
+    }
 }
