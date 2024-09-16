b2373e99864720bfbd1f0484776f20e43a29a83d
jiaQiu9 <351470636@qq.com>
2024-09-16 11:44:28 -0400

    created enterboardsize() to get the board size from the user with a min of 3x3

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int numPlayer;
         int boardSize;
         int userGame;
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello and Welcome!");
 
         // check which game the user wants to play
         userGame = gameChoice();
@@ -19,28 +19,11 @@ public class Main {
         if (userGame==1){
             // for tic-tac-toe
             // get the number of players
-
-            System.out.print("Please enter the number of players(Enter numeric values): ");
-            String userIn = userInput.nextLine();
-            System.out.println();
-            while(checkInt(userIn)){
-                System.out.print("Please enter the number of players(Enter a numeric value): ");
-                userIn = userInput.nextLine();
-                System.out.println();
-            }
-            numPlayer = Integer.parseInt(userIn);
-            System.out.println("The number of players are "+numPlayer);
+            // get the number of players, the user should enter an even number
+            numPlayer = getNumPlayer();
 
             // get the size of the board in terms of nxn, by now
-            System.out.print("Please enter the size(nxn size) of the board :(Enter a numeric value)  ");
-            userIn = userInput.nextLine();
-            while(checkInt(userIn)){
-                System.out.print("Please enter the size of the board :(Enter a numeric value)  ");
-                userIn = userInput.nextLine();
-                System.out.println();
-            }
-            boardSize = Integer.parseInt(userIn);
-            System.out.println("The size of the board is "+boardSize);
+            boardSize = enterBoardSize();
 
         }
         else{
@@ -55,49 +38,86 @@ public class Main {
 
         }
 
-
-
     }
+
+
+    // -----------------------------------------------------------------------------------------------------------------
     // this method will be used more than once
     public static boolean checkInt(String userIn){
         boolean isValid=false;
         try{
             int num = Integer.parseInt(userIn);
+            isValid = true;
         }
         catch(NumberFormatException e){
             isValid=false;
         }
         return isValid;
     }
+
+    // -----------------------------------------------------------------------------------------------------------------
     public static int gameChoice(){
         Scanner userInput = new Scanner(System.in);
         System.out.println("Please choose the game you want to play.");
         System.out.println("1.Tic-Tac-Toe");
         System.out.println("2. Order or Chaos");
-        //System.out.print("Choose your game:(Enter numeric values)");
         String userIn="";
-        boolean isValid = true; // for the while loop to run
-        while(isValid){
+        boolean isValid = false; // for the while loop to run
+        while(!isValid){
             System.out.print("Choose your game:(Enter numeric values)");
             userIn = userInput.nextLine();
             System.out.println();
             if (checkInt(userIn)){
                 if (Integer.valueOf(userIn)==1||Integer.valueOf(userIn)==2){
+                    isValid = true;
+                }
+            }
+        }
+        return Integer.valueOf(userIn);
+    }
+    // -----------------------------------------------------------------------------------------------------------------
+    public static int getNumPlayer(){
+        // the number of player based on the user input
+        // some games have fixed player num, some have flexible player num
+        // the number of player should be even number, for now
+        Scanner userInput = new Scanner(System.in);
+        String userIn="";
+        int numPlayer = 0;
+        boolean isValid = false;
+        while(!isValid){
+            System.out.print("Please enter the number of players(Enter numeric values, the number of players should be an even number): ");
+            userIn = userInput.nextLine();
+            System.out.println();
+            if (checkInt(userIn)){
+                if (Integer.valueOf(userIn)%2==0){
+                    isValid = true;
+                    numPlayer = Integer.parseInt(userIn);
+                }
+            }
+        }
+        return numPlayer;
+
+    }
+    // -----------------------------------------------------------------------------------------------------------------
+    public static int enterBoardSize(){
+        Scanner userInput = new Scanner(System.in);
+        String userIn="";
+        boolean isValid = false;
+        int boardSize = 3;
+        while(!isValid){
+            System.out.print("Enter the size of the board: (Enter one numeric value, min size is 3x3):");
+            userIn = userInput.nextLine();
+            if (checkInt(userIn)){
+                if (Integer.valueOf(userIn)<3){
                     isValid = false;
+                }
+                else if (Integer.valueOf(userIn)>=3){
+                    isValid = true;
                 }
             }
         }
 
-        return Integer.valueOf(userIn);
-    }
-
-    public static int getNumPlayer(){
-        // the number of player based on the user input
-        // some games have fixed player num, some have flexible player num
-        Scanner userInput = new Scanner(System.in);
-
-        return 0;
-
+        return boardSize;
     }
 
 }
