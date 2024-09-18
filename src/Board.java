808442536650669a1db6292dc1a43d7f188a2620
jiaQiu9 <351470636@qq.com>
2024-09-18 00:27:55 -0400

    got the player to set mark on the board, need to distinguish between win and board filled out, and change player of the same team

--- src/Board.java
+++ src/Board.java
@@ -1,20 +1,25 @@
 public class Board {
-    private int Turn; // keep track of player trun
+
     private Box[][] Boardsur; //store separate class for each box on the board
-    private String[][] playerWinLoss; // maybe there is no need for this
-    private String[][] moves; //
     private int boardSizeM; //store the row number of the board
     private int boardSizeN; //store the column number of the board
+    private int occupancy; // if this value is equal to the number of boxes on the board, then the board is filled out
+
+    private String[][] playerWinLoss; // maybe there is no need for this
+    private String[][] moves; //
+    private int Turn; // keep track of player trun
+
 
     public Board(int boardM, int boardN){
         this.boardSizeM=boardM;
         this.boardSizeN=boardN;
         this.Boardsur=new Box[this.boardSizeM][this.boardSizeN];
+        this.occupancy=0; // no moves on the board yet
     }
     // creates boxes objects on the board with unique id
     public void createBoardPiece(){
         int count = this.getBoardSizeM() * this.getBoardSizeN();
-        int current = 0;
+        int current = 0; // the id assigned to the current box object
         for (int i=0;  i<this.getBoardSizeM();i++){
             for (int j=0; j<this.getBoardSizeN(); j++){
                 if (current<count){
@@ -24,6 +29,7 @@ public class Board {
             }
         }
     }
+
     public int getBoardSizeM(){
         return this.boardSizeM;
     }
@@ -52,7 +58,26 @@ public class Board {
             System.out.println();
         }
     }
+    // return the entire list of boxes
     public Box[][] getBoardsur(){
         return this.Boardsur; // return the box list on the board.
+    }
+
+    // return the box object at position n, which is the numeric presentation of the box on the board
+    // the box with id of position
+    public Box getBoxApos(int pos){
+        int row = pos / this.boardSizeM;
+        int col = pos % this.boardSizeM;
+        return this.Boardsur[row][col];
+    }
+
+    // update the occupancy
+    public void increaseOccupancy(){
+        this.occupancy++;
+    }
+
+    // get occupancy
+    public int getOccupancy(){
+        return this.occupancy;
     }
 }
--- src/GameLogic.java
+++ src/GameLogic.java
@@ -17,24 +17,18 @@ public class GameLogic {
         // get the number of players
         // get the number of players, the user should enter an even number
         numPlayer = getNumPlayer();
-        // use the numplayer to create numplayer Player objects
-        Player[] players= new Player[numPlayer];
+        // use the numplayer(number of players) to create a list of type player with size numplayer
+        Player[] playerslst= new Player[numPlayer];
 
         if (userGame==1){
             // for tic-tac-toe
 
-            setPlayerMarks3t(players, numPlayer);
-
-            // get the size of the board in terms of nxn, by now
-            boardSize = enterBoardSize();
-
-            // set the board and pieces of the board
-            Board tictac = new Board(boardSize, boardSize);
-            tictac.createBoardPiece();
-            tictac.printBoard();
+            // create numplayer player objects, which should be stored in playerslst
+            // the numplayer would be fixed from this point on, until the players end the game. easier to keep track of players
+            setPlayerMarks3t(playerslst, numPlayer);
 
             // Game play
-            gamePlay3t(tictac, players, numPlayer);
+            gamePlay3t( playerslst, numPlayer);
 
 
         }
@@ -42,11 +36,12 @@ public class GameLogic {
             // for order and chaos
             System.out.println("Order and chaos Launching");
             boardSize = 6;
-            System.out.println("The size of the board is "+boardSize);
+            System.out.println("The size of the board is "+boardSize+", which is fixed for the current Game.");
             Board orderChaos=new Board(boardSize,boardSize);
             orderChaos.createBoardPiece();
             orderChaos.printBoard(); // initial printout of the board game
 
+            // Game Play
         }
     }
 
@@ -121,13 +116,14 @@ public class GameLogic {
         boolean isValid = false;
         int boardSize = 3;
         while(!isValid){
-            System.out.print("Enter the size of the board: (Enter one numeric value, min size is 3x3):");
+            // the max could be changed later
+            System.out.print("Enter the size of the board: (Enter one numeric value, min size is 3, max is 20):");
             userIn = userInput.nextLine();
             if (checkInt(userIn)){
                 if (Integer.valueOf(userIn)<3){
                     isValid = false;
                 }
-                else if (Integer.valueOf(userIn)>=3){
+                else if (Integer.valueOf(userIn)>=3 && Integer.valueOf(userIn)<=20){
                     isValid = true;
                 }
             }
@@ -218,39 +214,130 @@ public class GameLogic {
                 players[i].setPlayerMark("O");
             }
         }
+    }
 
 
+    //------------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // need to check if it is an int value, check out of bounds, and check availability
+    public static void placeMark(Board board, Player player, int maxPos){
+        boolean isValid =false;
+        Scanner scanner = new Scanner(System.in);
+        String userIn;
+        int position;
+        while(!isValid){
+            System.out.println("Player "+ player.getPlayerID()+ " make your move. Your mark is "+ player.getPlayerMark());
+            System.out.print("Enter a numeric value that represents the position on the board, that your want to place your mark:");
+            board.printBoard();
+
+            userIn = scanner.nextLine();
+            if (checkInt(userIn)){
+                //it is an integer value, need to check if the entered position is within the board parameter
+                position = Integer.parseInt(userIn);
+                if (position<1 || position>=maxPos){
+                    // out of bounds
+                    isValid = false;
+                    System.out.println("Position is out of bounds. Try again.");
+                }
+                else if (position>=1 && position<maxPos){
+                    // in bounds
+                    // need to check availability/ if empty, then assign it
+                    if (board.getBoxApos(position).isOccupied()){
+                        System.out.println("Position is occupied. Try again.");
+                    }else{
+                        //set the box at position on the board to be occupied
+                        board.getBoxApos(position).setOccupied(true);
+                        //set the mark on box at position on the board to be the current player's mark
+                        board.getBoxApos(position).setMark(player.getPlayerMark());
+                        board.increaseOccupancy();
+                        isValid = true; // end the loop, outside the method need to check win condition
+                    }
+                }
+
+            }
+            else{
+                System.out.println(" Please Enter a numeric value");
+                isValid=false;
+            }
+        }
     }
 
     // -----------------------------------------------------------------------------------------------------------------
     // -----------------------------------------------------------------------------------------------------------------
     //Game Play for tic-tac-toe
-    public static void gamePlay3t(Board board, Player[] players, int numPlayers){
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Tic-Tac-Toe Starting.");
+    public static void gamePlay3t( Player[] players, int numPlayers){
         //pointers for the players, so there is no need for creating two array to store players of different teams
         int first =0; // the pointed for the first player, in the first team
-        int second=1; // the pointer for the second player OR the first player of the other(second) team
+        int second=1; // the pointer for the second player OR the first player of the other(second) team, initialize to 1, modified later
         boolean gameEnd=false;
-        boolean turn = false;
+        boolean turn = false; //when false team 1's term . when true team 2's turn
         String playerMove = ""; // not sure what this is for.
+
+        int boardSize = 3; // the board size for 3t game starts from 3x3 to nxn
+
+        // this is where the start of the game
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Tic-Tac-Toe Starting.");
+
         // distinguish different num of players, for 2 players one will be X other will be O
         // for more than 2 player, the first half of the players will be X, the other half will be O
         // number of Players should always be even, and more than one
         if (numPlayers>2){
             second = numPlayers/2 -1;
         }
+        System.out.println("Game Start!");
 
+
+        // gameEnd is for when the player(s) do not want to play anymore, so ends the while loop
+        // the loop will continue as long as the player continues to play
         while(!gameEnd){
-            System.out.println("Game Start!");
-            board.printBoard();
-            if (turn){
-                System.out.println("Player "+ players[first].getPlayerID()+ " make your move. Your mark is "+ players[first].getPlayerMark());
-                System.out.print("Enter a numeric value that represents the position on the board, that your want to place your mark:");
+            boolean winNext = false; // false board not filled and no winner yet, true winner or the board is filled no winner
 
+            // get the size of the board in terms of nxn, by now, maybe in the future have mxn or every row has different column sizes
+            boardSize = enterBoardSize();
 
+            // set the board and pieces of the board
+            // new board, new board size at each game
+            Board tictac = new Board(boardSize, boardSize);
+            tictac.createBoardPiece();
+            tictac.printBoard();
+            int maxNumBox=boardSize*boardSize ;
+            // after creating the board, and pieces on the board.
+            // have a while loop that keeps altering the turns, ask players for moves, ends when either the board is filled with no winner
+            // or when there is a winner
+
+            while(!winNext){
+                // when turn== true, the first team moves. when turn == false, the second team moves
+                if (turn){
+                    // maxNumbox-1, positions start at 0, not 1
+                    placeMark(tictac, players[first], maxNumBox-1);
+                    winNext = CheckWin(tictac,players[first].getPlayerMark());
+                    if (winNext == false){
+                        // when the current player's move is not a win condition
+                        turn = !turn; // change turn
+                    }
+                    if(tictac.getOccupancy() == maxNumBox){
+                        // the board is filled out, end game
+                        winNext = true;
+                        // TO-DO need to distinguish between win and board filled out
+                    }
+                    if (numPlayers>2){
+                        // TO-DO need something to change player in the same team
+                    }
+
+                }
+                else{
+                    placeMark(tictac, players[second], maxNumBox-1);
+                    winNext = CheckWin(tictac,players[second].getPlayerMark());
+                }
             }
+
         }
+
+
+        // TO-DO, need to display player results, this is where the players have done with the game
+
+
     }
 
     // -----------------------------------------------------------------------------------------------------------------
